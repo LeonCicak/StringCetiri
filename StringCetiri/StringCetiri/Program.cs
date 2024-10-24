4312ecc021e362fea95d9a8fa17b8adf8c497038
LeonCicak <leoncicak07@gmail.com>
2024-10-24 14:56:28 +0200

    Add files via upload

---
+++ StringCetiri/StringCetiri/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCetiri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Unesite rečenicu: ");
+            string recenica = Console.ReadLine();
+
+            
+            string[] rijeci = recenica.Split(' ');
+
+            
+            if (rijeci.Length > 0)
+            {
+                
+                string prvaRijec = rijeci[0];
+                
+                string zadnjaRijec = rijeci[rijeci.Length - 1];
+
+                
+                Console.WriteLine("Prva riječ: " + prvaRijec);
+                Console.WriteLine("Zadnja riječ: " + zadnjaRijec);
+            }
+            else
+            {
+                Console.WriteLine("Niste unijeli važeću rečenicu.");
+            }
+        }
+    }
+}
